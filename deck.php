c14c74203ecaaaa3a3f00c5b60339e445006ec83
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-11-12 13:15:10 +0000

    added DocBlock to Deck Class

--- deck.php
+++ deck.php
@@ -13,23 +13,51 @@ require 'card.php';
  */
 class Deck
 {
-    public $numberOfCards; //std 54 with two jokers 52 std
-    public $numberOfPacks; //std 1
-    public $cards; //array
-    //    public $containsJokers = false; //true or false
+    /**
+     * @var int $numberOfCards number of cards in one pack - std 52
+     */
+    public $numberOfCards;
+    /**
+     * @var int $numberOfPacks number of packs in a deck - std 1
+     */
+    public $numberOfPacks;
+    /**
+     * @var array $cards contains array of card objects
+     */
+    public $cards;
+    /**
+     * @var array $faces contains faces for Card object
+     */
     public $faces = array('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King');
+    /**
+     * @var array $suits contains suits for Card object
+     */
     public $suits = ['heart', 'spade', 'diamond', 'club'];
+    //TODO:allow to create jokers card object 2 per std pack (red. black)
+    //    public $containsJokers = false; //true or false
 
+    /**
+     * constructs a deck depending of number packs passed in
+     * sets number of packs property
+     * sets number of cards property
+     *
+     * @param int $numberOfPacks number of packs required to build a deck
+     */
     public function __construct($numberOfPacks = 1)
     {
 
-        for ($i = 1; $i<=$numberOfPacks; $i++) {
-            $deck[] = array($i=>$this->createPack());
+        for ($i = 1; $i <= $numberOfPacks; $i++) {
+            $deck[] = array($i => $this->createPack());
         }
         $this->numberOfPacks = $numberOfPacks;
         $this->numberOfCards *= $numberOfPacks;
     }
 
+    /**
+     * Creates pack of card object each value of each suit and returns it in an array
+     *
+     * @return array
+     */
     public function createPack()
     {
         $cards = [];
@@ -44,17 +72,32 @@ class Deck
         return $cards;
     }
 
+    /**
+     * Shuffles array of cards
+     */
     public function shuffleCards()
     {
         shuffle($this->cards);
     }
 
+    /**
+     * Picks a random card form cards array and returns am array containing its face and suit
+     *
+     * @return mixed
+     */
     public function pickACard()
     {
         $cards = $this->cards;
         $randomCardIndex = array_rand($this->cards);
+
         return $cards[$randomCardIndex];
     }
+
+    /**
+     * Removes given card from an array of cards
+     *
+     * @param $card
+     */
     public function removeACard($card)
     {
         $cards = $this->cards;
